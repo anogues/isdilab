2c418d5e02e0d39510e7ed6bf8515d05a605eed1
anogues <anogues@yahoo.es>
2025-05-01 12:38:47 +0200

    Create providers.tf

---
+++ terraform/providers.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = "~> 1.05" 
+    }
+  }
+  cloud {
+    organization = "anogues-ISDI" # Reemplaza con el nombre de tu org de TFC
+
+    workspaces {
+      name = "dbt-snowflake-lab"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+provider "snowflake" {
+  # Credenciales obtenidas de variables de entorno (SNOWFLAKE_USER, SNOWFLAKE_PASSWORD, etc.)
+  # La región se especifica vía variable por claridad si se necesita en otro lugar
+  # role = var.snowflake_role # A menudo también se establece vía variable de entorno
+}
